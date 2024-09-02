1a2ba4f30b5dc5f225ef8901c744b9f413e39bd1
Dennis <dennisbircan@gmail.com>
2024-09-02 17:17:04 +0200

    Metod10

---
+++ Metoder10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Metoder10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string returnValue = "this is fun";
+            Translate(ref returnValue);
+
+            Console.WriteLine(returnValue);
+
+            Console.ReadLine();
+
+        }
+
+        static void Translate(ref string value)
+        {
+            
+            string[] consonants = { "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"};
+
+            for (int i = 0; i < consonants.Length; i++)
+            {
+
+                if (value.Contains(consonants[i]))
+                {
+                    value = value.Replace(consonants[i], consonants[i] + "o" +
+                    consonants[i]);
+                }
+            }
+        }
+
+    }
+}
