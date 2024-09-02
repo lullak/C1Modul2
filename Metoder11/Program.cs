6530e5e490205019b5e7522413157d759e16563f
Dennis <dennisbircan@gmail.com>
2024-09-02 17:20:22 +0200

    Metoder11

--- Metoder11/Program.cs
+++ Metoder11/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 1;
+            int y = 0;
+            Console.WriteLine($"x: {x}, y: {y}");
+            Translate(ref x, out y);
+            Console.WriteLine($"x: {x}, y: {y}");
+            Console.ReadLine();
+        }
+        static void Translate(ref int x, out int y)
+        {
+            x = 10 + x;
+            y = 20;
         }
     }
 }
