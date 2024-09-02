314a82e103beecd83ebe073841fa9fd65d1310b8
Dennis <dennisbircan@gmail.com>
2024-09-02 15:48:42 +0200

    Metoder8

--- Metoder8/Program.cs
+++ Metoder8/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 1, 2, 3, 4};
+
+            int sumOfAddition = Sum(numbers);
+            int sumOfMultiplication = Multiply(numbers);
+
+            Console.WriteLine($"Summan av att addera är: {sumOfAddition}\nSumman av att multiplera är: {sumOfMultiplication}");
+            Console.ReadLine();
+        }
+        static int Sum(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+
+        static int Multiply(int[] nums)
+        {
+            int sum = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum *= nums[i];
+            }
+            return sum;
         }
     }
 }
