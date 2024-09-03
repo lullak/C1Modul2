97a7f6f74599e4469fdf3068a6bb0620d97ae560
lullak <dennisbircan@gmail.com>
2024-09-03 14:15:36 +0200

    Forms

--- frmLogin/Form1.cs
+++ frmLogin/Form1.cs
@@ -2,22 +2,25 @@ namespace frmLogin
 {
     public partial class Form1 : Form
     {
+     
         public Form1()
         {
             InitializeComponent();
+           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
             if (textBoxEmail.Text != "")
             {
-                Form form2 = new Form();
+                Form form2 = new Form2();
                 this.Hide();
                 form2.Show();
             }
             else
             {
-                MessageBox.Show("Ange Epost eller telefonnummer.");
+                MessageBox.Show("Ange giltig epost eller telefonnummer.");
             }
         }
     }
---
+++ frmLogin/Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace frmLogin
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxPassword = new TextBox();
+            label1 = new Label();
+            loginButton = new Button();
+            SuspendLayout();
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(211, 114);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(235, 27);
+            textBoxPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(211, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Lösenord";
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(215, 156);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(94, 29);
+            loginButton.TabIndex = 2;
+            loginButton.Text = "Nästa";
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += loginButton_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(loginButton);
+            Controls.Add(label1);
+            Controls.Add(textBoxPassword);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxPassword;
+        private Label label1;
+        private Button loginButton;
+    }
+}
