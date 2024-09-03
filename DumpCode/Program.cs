4359bed1f7bf8a261d10fb038323b5e8edb26f44
lullak <dennisbircan@gmail.com>
2024-09-03 10:17:09 +0200

    Repitition vecka 1 i dumpcode

--- DumpCode/Program.cs
+++ DumpCode/Program.cs
@@ -1,21 +1,74 @@
-﻿
+﻿//DateTime TimeSpan
 
-int x = 3;
-Assign3(ref x);
-Console.WriteLine(x);
-void Assign3(ref int x)
-{
-    x = x + 3;
-}
+DateTime date1 = DateTime.Now;
+DateTime date2 = date1.AddDays(1);
+
+Console.WriteLine(date1 + " " + date2);
 
 
-int y;
-Out3(out y);
-Console.WriteLine(y);
-void Out3(out int y)
-{
-    y = 3;
-}
 
 
 Console.ReadLine();
+//strängar
+//string emailAddress = "Lisa.Lind@nackademin.se";
+//int seperator = emailAddress.IndexOf("@");
+
+//string name = emailAddress.Substring(0, seperator);
+//string domain = emailAddress.Substring(seperator + 1);
+
+//string nameList = "Lisa Lind,Anna Karlberg,Emma Svensson";
+//string[] nameArray = nameList.Split(',');
+
+//foreach (string names in nameArray)
+//{
+//    Console.WriteLine(names);
+//}
+
+//Console.WriteLine(name);
+//Console.WriteLine(domain);
+
+
+
+
+//Arrayer
+
+//int number = 0;
+//int[] numberArray = new int[5];
+//numberArray[0] = 1;
+//numberArray[1] = 2;
+//numberArray[2] = 3;
+//numberArray[3] = 5;
+//numberArray[4] = 6;
+// Första positionen i en array har index 0
+// Arrayen är viktig för den är grunden för andra datastrukturer.
+
+//for (int i = 0; i < numberArray.Length; i++)
+//{
+//    Console.WriteLine(numberArray[i]);
+//}
+
+//foreach (int numbers in numberArray)
+//{
+//    Console.WriteLine(numbers);
+//}
+
+
+//int x = 3;
+//Assign3(ref x);
+//Console.WriteLine(x);
+//void Assign3(ref int x)
+//{
+//    x = x + 3;
+//}
+
+
+//int y;
+//Out3(out y);
+//Console.WriteLine(y);
+//void Out3(out int y)
+//{
+//    y = 3;
+//}
+
+
+//Console.ReadLine();
