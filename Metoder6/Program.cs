1bcbd0e9da37300db3c6dec88db95e69b33ee7f6
Dennis <dennisbircan@gmail.com>
2024-09-04 14:07:13 +0200

    Metoder 6 kommentarer

--- Metoder6/Program.cs
+++ Metoder6/Program.cs
@@ -8,9 +8,10 @@
             Console.WriteLine(maxValue);
             Console.ReadLine();
         }
-
+        
         static int MaxVarde2(int value1, int value2)
         {
+            // En if else, som retunerar största värdet i Maxvärde2 metoden.
             if (value1 > value2) 
             return value1;
             else 
@@ -18,6 +19,7 @@
         }
         static int MaxVarde3(int value1, int value2, int value3)
         {
+            // Samma sak här men det finns fler värden att kontrollera, så att den endast skickar med det största
             if (value1 > value2 && value1 > value3)
                 return value1;
             else if (value2 > value3 && value2 > value3)
@@ -27,6 +29,8 @@
         }
         static int KontrolleraMaxVarde(int value1, int value2, int value3)
         {
+            //kontrollerar om någon av värdena är noll, och i det fallet kallar på MaxVärde 2 metoden, kör även en if else så att den
+            // inte kallar MaxVarde2 med en parameter som innehåller 0. Annars kallar den på MaxVarde3 metoden.
             if (value1 == 0 || value2 == 0 || value3 == 0)
                 if (value1 == 0)
                     return MaxVarde2(value2, value3);
---
+++ frmResize/Form1.cs
@@ -0,0 +1,10 @@
+namespace frmResize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
