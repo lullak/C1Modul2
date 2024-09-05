c7715ebda1e219e1d7c3279eadc05bbbd784b899
lullak <dennisbircan@gmail.com>
2024-09-05 20:35:02 +0200

    demo pizza

---
+++ DemoPizzeria/Form1.cs
@@ -0,0 +1,15 @@
+namespace DemoPizzeria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int CalculateChartSum()
+        {
+
+        }
+    }
+}
